c98b104ded356c565b410d47aefdd1413b5d8004
saydhans <saydhans@LIN-5CG4464M2F>
2025-02-12 12:31:36 +0530

    Add project files.

---
+++ HelloApplication/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("We are learning GIT");
+        }
+    }
+}
